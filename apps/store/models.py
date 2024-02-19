ecb571b48453258b581b1e5a9445e36ddea1f55c
Volodya-Sargsyan79 <volsarg@gmail.com>
2024-02-19 22:03:17 +0400

    create category models

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,3 +1,13 @@
+from django.contrib import admin
 from django.db import models
+from django.utils.html import format_html
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'{self.title}'
